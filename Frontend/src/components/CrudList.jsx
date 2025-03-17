1172ec449fbf39f7019d53cce896e13d4fb1b6b3
ranjithmca17 <ranjithmca007@gmail.com>
2025-03-17 10:33:12 +0530

    first commit

--- Frontend/src/components/CrudList.jsx
+++ Frontend/src/components/CrudList.jsx
@@ -29,4 +29,45 @@ const CrudList = ({ cruds, onDelete }) => {
           {cruds.map((crud, index) => (
             <tr key={crud._id}>
               <td>{index + 1}</td>
-              <t
+              <td>{crud.name}</td>
+              <td>{crud.email}</td>
+              <td>
+                <div className="input-group">
+                  <input
+                    type={showPassword[crud._id] ? "text" : "password"}
+                    className="form-control"
+                    value={crud.password}
+                    readOnly
+                  />
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    onClick={() => togglePasswordVisibility(crud._id)}
+                  >
+                    {showPassword[crud._id] ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+              </td>
+              <td>
+                <Link
+                  to={`/edit/${crud._id}`}
+                  className="btn btn-warning btn-sm me-2"
+                >
+                  <FaEdit />
+                </Link>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => onDelete(crud._id)}
+                >
+                  <FaTrash />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CrudList;
--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-4">
-      <p className="mb-0">&copy; 2023 CRUD App. All rights reserved. | Created by Ranjith</p>
+      <p className="mb-0">&copy; 2025 CRUD App. All rights reserved. | Created by Ranjith</p>
     </footer>
   );
 };
